237b5f3cbaadf9ede6aa665c4a05bf483d8abcfb
John Romby <au518762@uni.au.dk>
2018-09-26 18:00:24 +0200

    C# WPF UI Tutorials: 01 - Complete!

--- WpfBasics/MainWindow.xaml.cs
+++ WpfBasics/MainWindow.xaml.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfBasics
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            FinishComboBox_OnSelectionChanged(FinishComboBox, null);
+            PurchaseInfoComboBox_OnSelectionChanged(FinishComboBox, null);
+        }
+
+        private static void MyDebug(string x)
+        {
+            MessageBox.Show($"{x} clicked!");
+        }
+
+        private void ApplyBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            //MyDebug((string)((Button)sender).Content);
+            MessageBox.Show($"The description is: {DescriptionTxtBox.Text}");
+        }
+
+        private void ResetBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            WeldChkBox.IsChecked = AssemblyChkBox.IsChecked = PlasmaChkBox.IsChecked = LaserChkBox.IsChecked = PurchaseChkBox.IsChecked = LatheChkBox.IsChecked = DrillChkBox.IsChecked = FoldChkBox.IsChecked = RollChkBox.IsChecked = SawChkBox.IsChecked = false;
+        }
+
+        private void ChkBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            LengthTxtBox.Text += ((CheckBox) sender).Content + "  ";
+        }
+
+        private void FinishComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NoteTxtBox == null)
+                return;
+    
+            var combo = (ComboBox) sender;
+            var value = (ComboBoxItem) combo.SelectedValue;
+
+            NoteTxtBox.Text = (string) value.Content;
+        }
+
+        private void PurchaseInfoComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Not implemented yet!
+        }
+
+        private void SupplierNameTxtBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            MassTxtBox.Text = SupplierNameTxtBox.Text;
         }
     }
 }
